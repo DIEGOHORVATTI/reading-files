2ba0a6ffc2915ff99eb1467689d48573fe2ad466
DIEGOHORVATTI <d.horvattid@gmail.com>
2025-01-28 10:59:00 -0300

    refactor: update main.tsx formatting, remove unused CSS files, and enhance App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-
 import { parse } from 'papaparse';
-
 import { read, utils } from 'xlsx';
 
 type DataRow = {
@@ -56,43 +54,98 @@ const App = () => {
     }
   };
 
+  const fileUploadLoader = (
+    <p className="text-gray-500 mt-4">Carregando arquivo...</p>
+  );
+  const dataTable = (
+    <table className="table-auto w-full border-collapse border border-gray-100 mt-4 rounded-lg shadow-sm">
+      <thead>
+        <tr className="bg-gray-100 text-left">
+          <th className="border px-4 py-2 text-sm font-semibold text-gray-700">
+            Nome
+          </th>
+          <th className="border px-4 py-2 text-sm font-semibold text-gray-700">
+            Telefone
+          </th>
+          <th className="border px-4 py-2 text-sm font-semibold text-gray-700">
+            Endereço
+          </th>
+          <th className="border px-4 py-2 text-sm font-semibold text-gray-700">
+            CPF
+          </th>
+          <th className="border px-4 py-2 text-sm font-semibold text-gray-700">
+            ID
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {data.map((row, index) => (
+          <tr
+            key={index}
+            className="hover:bg-gray-50 cursor-pointer transition-colors duration-200 ease-in-out"
+          >
+            <td className="border px-4 py-2 text-sm">{row.nome}</td>
+            <td className="border px-4 py-2 text-sm">{row.telefone}</td>
+            <td className="border px-4 py-2 text-sm">{row.endereço}</td>
+            <td className="border px-4 py-2 text-sm">{row.cpf}</td>
+            <td className="border px-4 py-2 text-sm">{row.id}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  const noFileUploadedMessage = (
+    <p className="text-gray-500 mt-4">Nenhum arquivo carregado.</p>
+  );
+
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold mb-4">Carregar e Exibir CSV/Excel</h1>
-      <input
-        type="file"
-        accept=".csv, .xlsx, .xls"
-        onChange={handleFileUpload}
-        className="mb-4"
-      />
-      {loading ? (
-        <p className="text-gray-500">Carregando arquivo...</p>
-      ) : data.length > 0 ? (
-        <table className="table-auto w-full border-collapse border border-gray-200">
-          <thead>
-            <tr className="bg-gray-100">
-              <th className="border px-4 py-2">Nome</th>
-              <th className="border px-4 py-2">Telefone</th>
-              <th className="border px-4 py-2">Endereço</th>
-              <th className="border px-4 py-2">CPF</th>
-              <th className="border px-4 py-2">ID</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((row, index) => (
-              <tr key={index} className="hover:bg-gray-50">
-                <td className="border px-4 py-2">{row.nome}</td>
-                <td className="border px-4 py-2">{row.telefone}</td>
-                <td className="border px-4 py-2">{row.endereço}</td>
-                <td className="border px-4 py-2">{row.cpf}</td>
-                <td className="border px-4 py-2">{row.id}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p className="text-gray-500">Nenhum arquivo carregado.</p>
-      )}
+    <div className="h-screen font-sans text-gray-500 bg-gray-200">
+      <div className="flex justify-center w-full mx-auto sm:max-w-lg">
+        <div className="flex flex-col items-center justify-center w-full h-auto my-20 bg-white sm:w-3/4 sm:rounded-lg sm:shadow-xl">
+          <div className="mt-10 mb-10 text-center">
+            <h2 className="text-2xl font-semibold mb-2">
+              Carregar seus arquivos
+            </h2>
+            <p className="text-xs text-gray-500">Formato: .csv ou .xlsx</p>
+          </div>
+
+          <form
+            action="#"
+            className="relative w-4/5 h-32 max-w-xs mb-10 bg-white bg-gray-100 rounded-lg shadow-inner"
+          >
+            <input
+              type="file"
+              id="uploadFile1"
+              className="hidden"
+              onChange={handleFileUpload}
+            />
+            <label
+              htmlFor="uploadFile1"
+              className="z-20 flex flex-col-reverse items-center justify-center w-full h-full cursor-pointer"
+            >
+              <p className="z-10 text-xs font-light text-center text-gray-500">
+                Arraste & Solte seus arquivos aqui
+              </p>
+              <svg
+                className="z-10 w-8 h-8 text-indigo-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"></path>
+              </svg>
+            </label>
+          </form>
+        </div>
+      </div>
+
+      {loading
+        ? fileUploadLoader
+        : data.length > 0
+        ? dataTable
+        : noFileUploadedMessage}
     </div>
   );
 };
